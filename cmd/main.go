e6d898ab721f996976406ae87ad8cf0ca4029d6b
ABHINAV-JHA-27 <jha.abhinav2710@gmail.com>
2025-02-01 16:36:15 +0530

    Implemented Initial Logic for Accepting Arguments and Initing the Basic Repo for GitX.

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello There!!")
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: gitx <command> [<args>...]\n")
+		os.Exit(1)
+	}
+
+	switch command := os.Args[1]; command {
+	case "init":
+		for _, dir := range []string{".gitx", ".gitx/objects", ".gitx/refs"} {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+			}
+		}
+
+		headFileContents := []byte("ref: refs/heads/main\n")
+		if err := os.WriteFile(".gitx/HEAD", headFileContents, 0644); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+		}
+
+		fmt.Println("Initialized gitx directory")
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
+		os.Exit(1)
+	}
 }
